2ec8d7356ce5ec1e66f1facc83886ea774256a46
Leonardo Varuzza <varuzza@gmail.com>
2011-02-27 22:47:07 -0300

    First fast reader code.

--- scala/ReadFasta.scala
+++ scala/ReadFasta.scala
@@ -2,23 +2,39 @@ import scala.io.Source
 
 case class Sequence(val id:String,val text:String)
 
-object FastaReader {
+object ReadFasta {
   
-  def readFasta(in:Source):List[Sequence] {
-    def readFasta0(header:String,lines:Iterator[String]) {
-      
-      do {
-	
-      }
-    }
-    val lines = in.getLines
+  def readFasta(in:Source):List[Sequence] = {
+    def readFasta0(header:String,
+		   lines:Iterator[String],
+		   acc:List[Sequence]): List[Sequence] = {
 
-    readFasta0(lines.next,lines)
+      val (seq,rest) = lines.partition(_.startsWith(">"))
+      println("# " + header + " " + seq.toArray.mkString(""))
+      if (rest.hasNext) { 
+	readFasta0(rest.next,rest,
+		   new Sequence(header,seq.toArray.mkString("")) :: acc)
+      } else {
+	acc
+      }
+
+    }
+
+    val lines = in.getLines();
+    var line = ""
+    
+    do {
+      line = lines.next
+    } while (!line.startsWith(">"))
+    readFasta0(line,lines,Nil)
   }
 
   def main(args:Array[String]) {
     if (args.length > 0) {
       val seqs = readFasta(Source.fromFile(args(0)))
+      println(seqs)
+    } else {
+      println("Missing arg")
     }
   }
 }
