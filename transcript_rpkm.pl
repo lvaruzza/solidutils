a0a8d108155da5e7da01865b682a0df76e6d1252
Leonardo Varuzza <varuzza@gmail.com>
2011-07-07 12:53:54 -0300

    Add Transcript_rpkm script

--- transcript_rpkm.pl
+++ transcript_rpkm.pl
@@ -14,7 +14,14 @@ sub parse_attr {
  return \%r;
 }
 
-my %h;
+my %c;
+
+my $header1 = <>;
+my $header2 = <>;
+
+my ($total) = ($header2 =~ /#number_of_mapped_reads=(\d+)/);
+
+print "Total mapped reads = $total\n";
 
 while(<>) {
     next if(/^#/);
@@ -26,28 +33,35 @@ while(<>) {
     my $type = $l[2];
     my $start = $l[3];
     my $end = $l[4];
-    my $len = $end-$start;
+    my $len = $end-$start+1;
     my $attr = parse_attr($l[8]);
     my $count = $l[5];
 
     my $t_id = $attr->{transcript_id};
     my $g_id = $attr->{gene_id};
+    my $gene = $attr->{gene_name};
     my $en = $attr->{exon_number};
 
     #print "$type $t_id $g_id $count $en\n";
 
-    #if ($type eq "exon"  ) {
-	#$h{$t_id} += $count;
-	$h{$start."-".$end} = $count;
-    #}
+    if ($type eq "exon") {
+	#print "$t_id $type $len $count\n";
+	$c{$t_id} += $count;
+	$l{$t_id} += $len;
+    }
 
     #print "attr = ",Dumper($attr),"\n";
     #print "==============\n";
 }
 
-while(my ($t,$c) = each(%h)) {
-    #print "$t\t$c\n";
-    $sum += $c;
-}
+my $omega = (1000.0*1000*1000)/$total;
 
-print "sum = $sum\n";
+print join("\t",qw(transcript_id count length rpkm)),"\n";
+
+while(my ($t,$c) = each(%c)) {
+    my $l = $l{$t};
+
+    my $rpkm =  $c*1.0/$l * $omega;
+
+    print join("\t",$t,$c,$l,$rpkm),"\n";
+}
